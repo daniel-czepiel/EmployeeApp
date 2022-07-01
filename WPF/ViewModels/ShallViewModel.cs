4da9ef3b692e2de81e883cb4d9dc2921968b4f9a
daniel-czepiel <daniel.czepiel1805@gmail.com>
2022-07-01 19:54:48 +0200

    Better naming

--- WPF/ViewModels/ShallViewModel.cs
+++ WPF/ViewModels/ShallViewModel.cs
@@ -7,21 +7,24 @@ namespace WPF.ViewModels
     public class ShallViewModel : MvxViewModel
     {
         private readonly IMvxNavigationService _navigationService;
-        public IMvxCommand Menu { get; set; }
-        public IMvxCommand ShowEmployees { get; set; }
-        public IMvxCommand AddEmployee { get; set; }
+        public IMvxCommand MenuWindow { get; set; }
+        public IMvxCommand ShowEmployeesWindow { get; set; }
+        public IMvxCommand AddEmployeeWindow { get; set; }
         public ShallViewModel(IMvxNavigationService navigationService)
         {
             _navigationService = navigationService;
-            Menu = new MvxCommand(GetMenu);
-            ShowEmployees = new MvxCommand(GetShowEmployee);
-            AddEmployee = new MvxCommand(GetAddEmployee);
+            MenuWindow = new MvxCommand(GetMenu);
+            ShowEmployeesWindow = new MvxCommand(GetShowEmployee);
+            AddEmployeeWindow = new MvxCommand(GetAddEmployee);
         }
+
+        #region Menu methods
         public async void GetMenu() =>
             await _navigationService.Navigate<ShallViewModel>();
         public async void GetShowEmployee() =>
             await _navigationService.Navigate<EmployeesViewModel>();
         public async void GetAddEmployee() =>
             await _navigationService.Navigate<AddNewPersonViewModel>();
+        #endregion
     }
 }
